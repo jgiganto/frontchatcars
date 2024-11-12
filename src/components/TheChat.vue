8e8f4eb5ed8eb7f8df5eeea74da8e1b11e051b42
STRATESYS\jesus.giganto <jesus.giganto@servexternos.repsol.com>
2024-11-12 23:54:31 +0100

    deploy3

--- src/components/TheChat.vue
+++ src/components/TheChat.vue
@@ -88,7 +88,7 @@
 			try {
 				// Realiza una solicitud POST a tu servidor backend
 				const response = await axios.post(
-					"https://appservicecarfrontvue2.azurewebsites.net/chat",
+					"https://appservicecarfrontvue01.azurewebsites.net/chat",
 					{
 						query: query,
 						prompt:
